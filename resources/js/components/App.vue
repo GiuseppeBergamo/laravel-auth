57ea5f2215fbfb907edd36cb13305ef248bb410b
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-09-26 19:43:47 +0200

    added app component

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content">
+        <div class="title m-b-md">
+            Work in progress
+            <h2>Sito in costruzione</h2>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
